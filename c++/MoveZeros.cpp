6a6108321de67c3bf21729c11f54ad37ebd5cade
Nan Du <Strideradu@users.noreply.github.com>
2017-08-21 20:28:58 -0400

    8/21刷题

---
+++ c++/MoveZeros.cpp
@@ -0,0 +1,30 @@
+// 19 ms
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int last = 0;
+        for (int cur = 0;cur<nums.size();cur++){
+            if(nums[cur]!=0){
+                swap(nums[last++], nums[cur]);
+            }
+        }
+    }
+};
+
+
+// 16 ms
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int j = 0;
+        // move all the nonzero elements advance
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                nums[j++] = nums[i];
+            }
+        }
+        for (;j < nums.size(); j++) {
+            nums[j] = 0;
+        }
+    }
+};
