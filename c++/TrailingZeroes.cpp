1ea129ff27ea6e94cf9453d4a5ecd72cd30c96fd
Nan Du <Strideradu@users.noreply.github.com>
2017-09-11 17:29:23 -0400

    911 刷题

---
+++ c++/TrailingZeroes.cpp
@@ -0,0 +1,11 @@
+// need to use long long for i
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        int result = 0;
+        for (long long i = 5; n/i > 0; i = i * 5){
+            result += n/i;
+        }
+        return result;
+    }
+};
